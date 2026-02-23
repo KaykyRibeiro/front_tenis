ae5537827667f152fe08a844bf0b2c44e0f285a9
João Pedro <joaoprl055@gmail.com>
2026-02-23 09:49:47 -0300

    início login e componentes

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,7 +1,18 @@
+ 
+
 export default function Login() {
  return (
-   <div>
-      <h1>Login</h1>
+   <div className="bg-green-800">
+      <div className="flex justify-between items-center ">
+        <div className="text-end p-8">
+          <h1 className="text-white">Tennis Ball Academia <br /> logo</h1>
+        </div>
+        <div className="text-start">
+          <button className="">
+            
+          </button>
+        </div>
+      </div>
    </div>
  );
 }

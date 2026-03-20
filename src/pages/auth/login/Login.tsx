7fb06b3538ff40744845cb8a1c665dafca4ef3e1
Kayky Ribeiro <kaykykaro04@gmail.com>
2026-03-20 18:03:36 -0300

    v2 - login com zod + react hook form

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,18 +1,42 @@
 //import { useLinkClickHandler } from "react-router-dom";
 //import Button from "../../../components/Buttons/button";
-import { useState } from "react";
+/*import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { VerifyLogin } from "../../../service/authService"
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";*/
 
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const loginSchema = z.object({
+    email: z.string().nonempty("O email é obrigatório").email("Formato de email inválido"),
+    senha: z.string().nonempty("A senha é obrigatória"),
+  })
+
+  type LoginFormData = z.infer<typeof loginSchema>;
 export default function Login() {
-  const navigate = useNavigate()
-  const [form, setForm] = useState({ email: "", senha: "", });
-  const [searchParams] = useSearchParams();
-  const redirect = searchParams.get("redirect");
+  //const navigate = useNavigate()
+  //const [form, setForm] = useState({ email: "", senha: "", });
+  /*const [searchParams] = useSearchParams();
+  const redirect = searchParams.get("redirect");*/
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      senha: "",
+    }
+  });
+
+
+  
+  const onSubmit = (data: LoginFormData) => {
+    console.log(data);
+  }
+
+
+  /*const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try{
@@ -28,10 +52,9 @@ export default function Login() {
     }catch (err: any) {
       console.log(err);
     }
-  }
+  }*/
 
   return (
-
     <div className="bg-[url(/public/banner-bg/bg-img-tenis.jpg)] bg-cover bg-center h-screen w-screen flex justify-center items-center">
       <div className="bg-black/20 backdrop-blur-sm p-2 md:p-10 rounded-2xl shadow-xl border border-white/10">
         <div className="flex justify-center items-center">
@@ -39,40 +62,35 @@ export default function Login() {
         </div>
         <form action=""
           //onSubmit={}
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
           className=" flex flex-col justify-center items-center ">
 
-          <div className="mt-5 md:mt-10">
-            <input type="text"
-              placeholder="Insira seu Usuário ou Telefone"
-              value={form.email}
-              onChange={(e) => setForm({ ...form, email: e.target.value })}
+          <div className="mt-5 md:mt-10 flex flex-col">
+            <input 
+              type="email"
+              placeholder="Insira seu Email"
+              //value={form.email}
+              {...register("email")}
               className="w-80 md:w-150 border-b border-white text-white placeholder:text-white/70 font-light text-lg focus:outline-none" />
+            <p className="absolute mt-8">
+              {errors.email && <span className="text-red-500 text-sm mt-1">{errors.email.message}</span>}
+            </p>
           </div>
 
-          <div className="mt-10">
+          <div className="mt-10 flex flex-col">
             <input type="password"
               placeholder="Insira sua Senha"
-              value={form.senha}
-              onChange={(e) => setForm({ ...form, senha: e.target.value })}
+              //value={form.senha}
+              {...register("senha")}
               className="w-80 md:w-150 border-b border-white  text-white placeholder:text-white/70 font-light text-lg focus:outline-none" />
+            <p className="absolute mt-8">
+              {errors.senha && <span className="text-red-500 text-sm mt-1">{errors.senha.message}</span>}
+            </p>
           </div>
           <p
             className="cursor-pointer w-full items-end text-end text-gray-smooth/80 font-light text-sm hover:text-gray-smooth mb-15"
           //</div>onClick={() => navigate("/recuperar-senha")} 
           >Esqueceu sua senha?</p>
-
-
-          {/*
-          <div className="text-white">
-            <Button
-              type="button"
-              onClick={() => navigate("/Cadastro")}
-              children="Não possui uma conta? Cadastre-se agora!"
-              className="font-medium text-white cursor-pointer"
-            />
-          </div>
-          */}
 
 
           <div className="mt-12">
--- src/pages/professores/Professores.tsx
+++ src/pages/professores/Professores.tsx
@@ -1,13 +1,79 @@
+import { Search, UserPlus } from 'lucide-react';
 import Sidebar from '../../components/navigation/Sidebar';
 import clsx from 'clsx';
 
 export default function Professores() {
-    return (
-        <div className={clsx('flex flex-1 flex-col h-screen bg-gray-smooth dark:bg-black')}>
-            <div>
+    const professores = [
+        { id: 1, nome: 'Professor A', disciplina: 'Matemática' },
+        { id: 2, nome: 'Professor B', disciplina: 'Português' },
+        { id: 3, nome: 'Professor C', disciplina: 'Ciências' },
+    ];
 
+    const handleAdicionarProfessor = () => {
+        // Lógica para adicionar um novo professor
+    }
+    const handlePesquisar = () => {
+        // Lógica para pesquisar professores
+    }
+
+    const totalProfessores = professores.length;
+    return (
+        <div className={clsx('flex flex-1 bg-gray-smooth dark:bg-black')}>
+            <div className='flex flex-col ml-16 w-screen h-screen p-5'>
+                <div className=' flex flex-row justify-between items-center w-full p-5 border-b border-gray-300 dark:border-gray-700'>
+                    <div className='flex flex-row items-center gap-2'>
+                        <h1 className='text-2xl font-light text-black-smooth dark:text-white'>Gestão de Professores</h1>
+                        <p className='text-sm text-gray-500 dark:text-gray-500 bg-gray-300 dark:bg-gray-800 py-0.5 px-2 rounded-2xl'>{totalProfessores} professores registrados</p>
+                    </div>
+                    <div>
+                        <button
+                            onClick={handleAdicionarProfessor}
+                            className='flex flex-row gap-2 px-4 py-2 bg-primary-color text-white rounded-2xl hover:bg-green-600 transition-colors duration-300'>
+                            <UserPlus />
+                            Adicionar Professor
+                        </button>
+                    </div>
+                </div>
+                <div className='grid grid-cols-2 gap-4 w-full mt-5'>
+                    <div className='flex flex-col'>
+                        <div className='flex flex-row justify-between '>
+                            <div className='flex flex-row w-8/12'>
+                                <input
+                                    className='w-full bg-gray-300 dark:bg-zinc-800 text-black-smooth dark:text-white rounded-l-xl px-4 py-2 focus:outline-none'
+                                    type="text"
+                                    placeholder="Pesquisar professores..."
+                                />
+                                <button 
+                                    className='px-4 py-2 bg-primary-color  text-white rounded-r-xl hover:bg-green-600 transition-colors duration-300'
+                                    onClick={handlePesquisar}
+                                >
+                                    <Search />
+                                </button>   
+                            </div>
+                            <div className=''>
+                                <select 
+                                    className='bg-gray-300 dark:bg-zinc-800 text-black-smooth dark:text-white rounded-sm px-4 py-2 focus:outline-none'
+                                    name="disciplina" 
+                                    id="disciplina">
+                                    <option value="">Todos</option>
+                                    <option value="matematica">Matemática</option>
+                                    <option value="portugues">Português</option>
+                                    <option value="ciencias">Ciências</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <div className='w-full mt-4'>
+                            
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
-            <Sidebar page="Professores"/>
+            <Sidebar page="Professores" />
         </div>
     );
 }
